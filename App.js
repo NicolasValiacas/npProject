47519ce935ae19d5acb4d711de7a01607f7e94ec
NicolasValiacas <valiacas.nicolas@gmail.com>
2022-02-02 12:53:06 +0100

    deuxieme

--- App.js
+++ App.js
@@ -1,12 +1,16 @@
+import { NavigationContainer } from '@react-navigation/native';
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import tw from "tailwind-rn"
+import { Button, StyleSheet, Text, View } from 'react-native';
+import { AuthProvider } from './hooks/useAuth';
+import StackNavigator from './StackNavigator';
 
 export default function App() {
   return (
-    <View style={styles.container}>
-      <Text>Welcome on Tindog</Text>
-    </View>
+    <NavigationContainer>
+      <AuthProvider>
+        <StackNavigator />
+      </AuthProvider>
+    </NavigationContainer>
   );
 }
 
---
+++ screens/HomeScreen.js
@@ -0,0 +1,20 @@
+import { View, Text } from 'react-native';
+import React from 'react';
+import { Button } from 'react-native-web';
+import { useNavigation } from '@react-navigation/native';
+
+const HomeScreen = () => {
+    const navigation = useNavigation();
+    
+  return (
+    <View>
+      <Text>I am the Homescreen</Text>
+      <Button 
+        title="Go to Chat Screen" 
+        onPress={() => navigation.navigate("Chat")} 
+      />
+    </View>
+  );
+};
+
+export default HomeScreen;
